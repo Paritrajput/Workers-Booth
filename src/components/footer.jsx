bb86eb152401e80acfd64a99822e87f71a040c90
Divya Kumari <divyamali0404@gmail.com>
2026-02-19 01:56:21 +0530

    Your commit message

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="flex justify-center relative bg-black ">
-      <div className="font-bold text-xl text-white">Made with ❤️️ by Team Vibhav</div>
+      <div className="font-bold text-50 text-white">Made with ❤️️ by Team Vibhav</div>
     </div>
   );
 }
